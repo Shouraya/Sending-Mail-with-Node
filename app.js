fd037df65fea51ffb731c7aaa2960312059ecc7d
Shouraya <shourayagoyal2406@gmail.com>
2020-10-09 12:23:21 +0530

    finally done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const nodemailer = require('nodemailer');
-// const hbs = require('nodemailer-express-handlebars')
+const hbs = require('nodemailer-express-handlebars');
 
 //Step 1
 
@@ -13,15 +13,22 @@ let transporter = nodemailer.createTransport({
     }
 });
 
+transporter.use('compile', hbs({
+    viewEngine: "express-handlebars",
+    viewPath: "./",
+    extName: ".handlebars"
+}));
+
 //Step 2
 
 let mailOptions = {
-    from: 'shourayagoyal2406@gmail.com',
+    from: 'shourayagoyal2000@gmail.com',
     to: 'sgoyal_be19@thapar.edu, shourayagoyal2406@gmail.com',
     // cc: ''
     // bcc: ''
     subject: 'Testing and Testing',
-    text: 'IT works'
+    text: 'IT works',
+    template: 'main'
 };
 
 //Step 3
@@ -31,5 +38,5 @@ transporter.sendMail(mailOptions)
         console.log('Email Sent !!');
     })
     .catch(function(error) {
-        console.log('Error: ' error);
+        console.log('Error: ' ,error);
     });
